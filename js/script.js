6ab026afd0ca2ef8425fbef8daa35a8976e79744
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-03-29 15:24:34 +0200

    start es 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
 /**
  * 
+ * Esercizio 2 - Pari e Dispari
+ * 
+ * - L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+ * - Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+ * - Sommiamo i due numeri
+ * - Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+ * - Dichiariamo chi ha vinto.
+ * 
+ */
+
+// Scelta Pari/Dispari
+
+
+
+
+var pariDispari = prompt('Quale numero vuoi inserire?\nPari\nDispari').toLowerCase().trim();
+
+
+
+switch(pariDispari){
+    case 'pari': 
+    var userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
+    console.log('Hai scelto: ', pariDispari)
+
+    if((userNumber >= 1 && userNumber <= 5) && (!isNaN(userNumber))){
+        console.log('correct');
+    } else console.log('errato');
+    break;
+}
+
+
+
+
+/**
+ * 
  *  Esercizio 1 - Parola Palindroma
  * 
  * - Chiedere all’utente di inserire una parola
@@ -8,12 +43,12 @@
 
 // Fase 1 Richiesta dato all'utente
 
-var userWord = prompt('Inserire la parola da verificare').toLowerCase().trim();
-console.log('Parola da verificare: ', userWord);
+// var userWord = prompt('Inserire la parola da verificare').toLowerCase().trim();
+// console.log('Parola da verificare: ', userWord);
 
 // Fase 2 Verifica se parola è Polindroma
 
-var wordPalindrome = reverseChars(userWord);
+// var wordPalindrome = reverseChars(userWord);
 
 
 
