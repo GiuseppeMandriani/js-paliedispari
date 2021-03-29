ae521e3d91802a1d4682babc136a4fc2bd8da0b8
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-03-29 14:01:05 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ *  Esercizio 1 - Parola Palindroma
+ * 
+ * - Chiedere all’utente di inserire una parola
+ * - Creare una funzione per capire se la parola inserita è palindroma
+ */
+
+
+
+
+/************************
+ * 
+ * UTILITIES
+ * 
+ *************************/
+
+// Definizione Funzioni
