faba9534d648c2d5c812eda0506eca08d9c50dd8
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-03-29 14:44:13 +0200

    esercizio 1 ok

--- js/script.js
+++ js/script.js
@@ -6,6 +6,14 @@
  * - Creare una funzione per capire se la parola inserita è palindroma
  */
 
+// Fase 1 Richiesta dato all'utente
+
+var userWord = prompt('Inserire la parola da verificare').toLowerCase().trim();
+console.log('Parola da verificare: ', userWord);
+
+// Fase 2 Verifica se parola è Polindroma
+
+var wordPalindrome = reverseChars(userWord);
 
 
 
@@ -16,3 +24,29 @@
  *************************/
 
 // Definizione Funzioni
+
+
+/**
+ * 
+ * @param {string} word //Parola da invertire
+ * @returns // Parola invertita
+ */
+function reverseChars(word){
+
+    var reverseWord = '';
+    
+    for (var i = word.length - 1; i >= 0; i--){
+        // console.log(word[i]) // Singola lettera della parola
+        reverseWord += word[i]; // Aggiunta lettera per lettera
+    }
+
+    if (reverseWord === word){
+        console.log('La Parola è Polindroma: ', reverseWord);
+        
+    } else console.log('Mi spiace, non è Polindroma: ', reverseWord)
+    
+    return reverseWord;
+
+
+
+}
